e452ff72383698fdc5c4d52bc054edcd1bf5587a
Mezi Aris Apronny <meziaris@gmail.com>
2020-06-13 13:02:50 +0700

    Create php7.4-nginx-mariadb.sh

---
+++ php7.4-nginx-mariadb.sh
@@ -0,0 +1,26 @@
+echo "INSTALL NGINX"
+echo "=============================>"
+sudo apt update && sudo apt upgrade -y
+sudo apt install nginx -y
+
+echo "UFW ENABLE NGINX HTTP & OpenSSH"
+echo "=============================>"
+sudo ufw enanble
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'OpenSSH'
+
+echo "INSTALL MARIADB"
+echo "=============================>"
+sudo apt-get install -y mariadb-server mariadb-client
+sudo mysql_secure_installation
+
+echo "INSTALL PHP"
+echo "=============================>"
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update -y
+sudo apt install -y php7.4
+
+echo "INSTALL modules for PHP"
+echo "=============================>"
+sudo apt install -y php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip
