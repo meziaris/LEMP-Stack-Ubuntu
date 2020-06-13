54e82b68510b7b2ab6de2ff62be6bb982ab84306
Mezi Aris Apronny <meziaris@gmail.com>
2020-06-13 13:41:12 +0700

    Update php7.4-nginx-mariadb.sh

--- php7.4-nginx-mariadb.sh
+++ php7.4-nginx-mariadb.sh
@@ -16,7 +16,7 @@ sudo mysql_secure_installation
 
 echo "INSTALL PHP 7.4"
 echo "=============================>"
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 sudo apt install -y php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip
