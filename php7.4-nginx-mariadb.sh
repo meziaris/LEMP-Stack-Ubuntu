a34d38bf9383d74266b084f683a6d6eda5a3f8d7
meziaris <meziaris@gmail.com>
2020-06-28 14:16:24 +0700

    update code apt

--- php7.4-nginx-mariadb.sh
+++ php7.4-nginx-mariadb.sh
@@ -8,11 +8,11 @@ sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'OpenSSH'
 
 #install mariadb
-sudo apt-get install -y mariadb-server mariadb-client
+sudo apt install -y mariadb-server mariadb-client
 sudo mysql_secure_installation
 
 #install php7.4 and modulus
-sudo apt-get install software-properties-common -y
+sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 sudo apt install -y php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip php7.4-gd php7.4-imagick php7.4-dev php7.4-imap php7.4-opcache php7.4-soap
