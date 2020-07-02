6995bc5ba5f1be6e30113284a914cf08dacf1171
Mezi Aris Apronny <meziaris@gmail.com>
2020-07-03 06:31:29 +0700

    Add php-bcmath

--- php7.4-nginx-mariadb.sh
+++ php7.4-nginx-mariadb.sh
@@ -9,4 +9,4 @@ sudo apt install -y mariadb-server mariadb-client
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
-sudo apt install -y php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip php7.4-gd php7.4-imagick php7.4-dev php7.4-imap php7.4-opcache php7.4-soap
+sudo apt install -y php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip php7.4-gd php7.4-imagick php7.4-bcmath php7.4-dev php7.4-imap php7.4-opcache php7.4-soap
